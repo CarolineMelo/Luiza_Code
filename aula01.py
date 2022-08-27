787c5d1065d34fab915adf91449a77f6fb6fe86c
Caroline <2015302@aluno.univesp.br>
2022-08-27 14:14:10 -0300

    aula01: variáveis

--- aula01.py
+++ aula01.py
@@ -1,5 +1,6 @@
 # Professora Milene Martins - Aula 01 - Luiza<Code>
 # Na lógica computacional, podem ser considerados como 0 ou 1.
+# Tipos:
 
 semana= False
 feriado = True
@@ -13,3 +14,11 @@ altura = 1.75
 print(type(nome))
 print(type(idade))
 print(type(altura))
+
+#variáveis
+
+sua_altura = 1.62
+print(sua_altura)
+
+frase_de_boas_vindas = 'Sejam bem vindas ao LuizaCode'
+print(frase_de_boas_vindas)
