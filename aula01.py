09dcee358ab4d59cb3df83149c593874e31718a7
Caroline <2015302@aluno.univesp.br>
2022-08-27 15:01:46 -0300

    aula01: operadores comparativos

--- aula01.py
+++ aula01.py
@@ -2,7 +2,7 @@
 # Na lógica computacional, podem ser considerados como 0 ou 1.
 # Tipos:
 
-semana= False
+semana = False
 feriado = True
 print(type(semana))
 print(type(feriado))
@@ -15,7 +15,7 @@ print(type(nome))
 print(type(idade))
 print(type(altura))
 
-#variáveis
+# variáveis
 
 sua_altura = 1.62
 print(sua_altura)
@@ -42,3 +42,20 @@ modulo = number1 % number2
 print(modulo)
 exponenciacao = number1 ** number2
 print(exponenciacao)
+
+
+# operadores comparativos
+variavel = 5
+
+if variavel == 5:
+    print('Os valores são iguais')
+if variavel != 7:
+    print('O valor da variavel não é igual a 7')
+if variavel > 2:
+    print('O valor da variavel é maior que 2')
+if variavel >= 5:
+    print('O valor da variavel é maior ou igual a 5')
+if variavel < 7:
+    print('O valor da variavel é menor a 7')
+if variavel <= 5:
+    print('O valor da variavel é menor ou igual a 5')
