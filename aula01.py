b4f9ff579ca215691adf80bc24931d617e27a60d
Caroline <2015302@aluno.univesp.br>
2022-08-27 13:39:43 -0300

    aula 01: testando

---
+++ aula01.py
@@ -0,0 +1,15 @@
+# Professora Milene Martins - Aula 01 - Luiza<Code>
+# Na lógica computacional, podem ser considerados como 0 ou 1.
+
+semana= False
+feriado = True
+print(type(semana))
+print(type(feriado))
+
+nome = 'Marcia'
+idade = 13
+altura = 1.75
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
